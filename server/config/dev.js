1bac6cf798b907d78d2bf16f58a84b85b8d61a0a
Patrick Rivera <pxr13@my.fsu.edu>
2018-04-16 00:53:38 -0400

    Added keys

---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+console.log(process.env.DEV_MONGO_URI);
+
+module.exports = {
+  mongoURI: process.env.DEV_MONGO_URI
+};
--- server/index.js
+++ server/index.js
@@ -1,23 +1,18 @@
 const mongoose = require('mongoose');
 const app = require('./app');
+require('dotenv').config();
+
+const keys = require('./config/keys');
 
 const port = process.env.PORT || 8080;
 
 mongoose.Promise = global.Promise;
 
 if (process.env.NODE_ENV !== 'test') {
-  mongoose
-    .connect(
-      'mongodb://patrickprod:password@ds143099.mlab.com:43099/notes-io-prod'
-    )
-    .then(() => {
-      console.log('Connected to database');
-    })
-    .catch((err) => {
-      console.log('Not connected to database!!!', err);
-    });
+  mongoose.connect(keys.mongoURI);
 }
 
+// Ensure only one server instance is running in test environment
 if (!module.parent) {
   app.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
 }
